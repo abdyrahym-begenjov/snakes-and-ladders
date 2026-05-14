ed8aecff73da1171d45cc3e573328e29bc8a9724
Abdyrahym Begenjov <abdurahymbegencev@gmail.com>
2026-05-14 21:34:01 +0500

    Version 1.1
    feat: implement player selection (2-4), computer opponents, and "Moment of Truth" mechanic

--- Snakes and Stairs.py
+++ Snakes and Stairs.py
@@ -1,6 +1,14 @@
 from random import randint
+from time import sleep
+c=['COMPUTER1', 'COMPUTER2', 'COMPUTER3']
 
-c='COMPUTER'
+def game(info):
+    name=input(f'[{info}] Enter name: ')
+    if name =='':
+        name=c[0]
+        c.pop(0)
+        return name
+    return name
 
 class Player:
     def __init__(self, name):
@@ -26,6 +34,14 @@ class Computer:
         self.level-=number
         return self.level
 
+while True:
+    count=input('Enter number of the players: ')
+    if count in ('2', '3', '4'):
+        count=int(count)
+        break
+    else:
+        print('Error!!!')
+
 parametr=50
 snakes=[13, 31]
 lsnakes=[47]
@@ -39,44 +55,104 @@ while True:
     else:
         print('Error!!!')
 
-Player2=input('[Player 2] Enter name: ')
-if Player2 =='':
-    Player2=c
+match count:
+    case 2:
+        Player2=game('Player 2')
+    case 3:
+        Player2=game('Player 2')
+        Player3=game('Player 3')
+    case 4:
+        Player2=game('Player 2')
+        Player3=game('Player 3')
+        Player4=game('Player 4')   
 
 while True:
-    u1=randint(1, 6)
-    u2=randint(1, 6)
-    lst=[(Player1, u1), (Player2, u2)]
-    lst.sort(key=lambda x: x[1], reverse=True)
-    result=list(map(lambda x: x[1], lst))
-    nr, r=[], []
-    for i in result:
-        if i not in nr:
-            nr.append(i)
-        else:
-            r.append(i)
-    if r==[]:
-        print(f'{Player1}: {u1}, {Player2}: {u2}')
-        break
-    else:
-        continue
+    match count:
+        case 2:
+            u1=randint(1, 6)
+            u2=randint(1, 6)
+            lst=[(Player1, u1), (Player2, u2)]
+            lst.sort(key=lambda x: x[1], reverse=True)
+            result=list(map(lambda x: x[1], lst))
+            nr, r=[], []
+            for i in result:
+                if i not in nr:
+                    nr.append(i)
+                else:
+                    r.append(i)
+            if r==[]:
+                print('Moment of Truth 🥁')
+                sleep(2)
+                print(f'{Player1}: {u1}, {Player2}: {u2}')
+                break
+            else:
+                continue
+        case 3:
+            u1=randint(1, 6)
+            u2=randint(1, 6)
+            u3=randint(1, 6)
+            lst=[(Player1, u1), (Player2, u2), (Player3, u3)]
+            lst.sort(key=lambda x: x[1], reverse=True)
+            result=list(map(lambda x: x[1], lst))
+            nr, r=[], []
+            for i in result:
+                if i not in nr:
+                    nr.append(i)
+                else:
+                    r.append(i)
+            if r==[]:
+                print('Moment of Truth 🥁')
+                sleep(3)
+                print(f'{Player1}: {u1}, {Player2}: {u2}, {Player3}: {u3}')
+                break
+            else:
+                continue
+        case 4:
+            u1=randint(1, 6)
+            u2=randint(1, 6)
+            u3=randint(1, 6)
+            u4=randint(1, 6)
+            lst=[(Player1, u1), (Player2, u2), (Player3, u3), (Player4, u4)]
+            lst.sort(key=lambda x: x[1], reverse=True)
+            result=list(map(lambda x: x[1], lst))
+            nr, r=[], []
+            for i in result:
+                if i not in nr:
+                    nr.append(i)
+                else:
+                    r.append(i)
+            if r==[]:
+                print('Moment of Truth 🥁')
+                sleep(6)
+                print(f'{Player1}: {u1}, {Player2}: {u2}, {Player3}: {u3}, {Player4}: {u4}')
+                break
+            else:
+                continue
 
 new_lst=list(map(lambda x: x[0], lst))
 result1=[]
 for i in new_lst:
-    if i=='COMPUTER':
+    if i in ['COMPUTER1', 'COMPUTER2', 'COMPUTER3']:
         result1.append(Computer(i))
     else:
         result1.append(Player(i))
 
-P1, P2=result1
+match count:
+    case 2:
+        P1, P2=result1
+    case 3:
+        P1, P2, P3=result1
+    case 4:
+        P1, P2, P3, P4=result1
 
 for n, i in enumerate(result1, 1):
     print(f'{n}) {i.name}')
 
 start1=input('Enter to start game: ')
-w='Winner'
-final_num=[1, 2]
+print('Let\'s Go!!!')
+w=['First Winner', 'Second Winner', 'Third Winner', 'Forth Winner']
+final_num=[1, 2, 3, 4]
+
 def brosok(obj):
     if obj.status==5:
         if isinstance(obj, Player):
@@ -88,8 +164,9 @@ def brosok(obj):
         obj.level+=num
         if obj.level==parametr:
             obj.status=final_num[0]
-            print(w)
+            print(w[0])
             final_num.pop(0)
+            w.pop(0)
         elif obj.level>parametr:
             print('Number is bigger than parametr')
             obj.level-=num
@@ -115,12 +192,47 @@ def brosok(obj):
 
 while True:
     P1.level, P1.status=brosok(P1)
-    P2.level, P2.status=brosok(P2)
-    spisok=[(P1.name, P1.level, P1.status), (P2.name, P2.level, P2.status)]
-    spisok.sort(key=lambda x: x[2], reverse=False)
-    spisok1=list(map(lambda x: x[0], spisok))
-    spisok2=list(map(lambda x: x[2], spisok))
-    if 1 in spisok2:
-        print(f'1) {spisok1[0]} - WINNER 😎🏆')
-        print(f'2) {spisok1[1]} - LOSER 😫')
-        break
+    P1.play=True
+    match count:
+        case 2:
+            P2.level, P2.status=brosok(P2)
+            P2.play=True
+            spisok=[(P1.name, P1.level, P1.status), (P2.name, P2.level, P2.status)]
+            spisok.sort(key=lambda x: x[2], reverse=False)
+            spisok1=list(map(lambda x: x[0], spisok))
+            spisok2=list(map(lambda x: x[2], spisok))
+            if 1 in spisok2:
+                print(f'1) {spisok1[0]} - WINNER 😎🏆')
+                print(f'2) {spisok1[1]} - LOSER 😫')
+                break
+        case 3:
+            P2.level, P2.status=brosok(P2)
+            P2.play=True
+            P3.level, P3.status=brosok(P3)
+            P3.play=True
+            spisok=[(P1.name, P1.level, P1.status), (P2.name, P2.level, P2.status), (P3.name, P3.level, P3.status)]
+            spisok.sort(key=lambda x: x[2], reverse=False)
+            spisok1=list(map(lambda x: x[0], spisok))
+            spisok2=list(map(lambda x: x[2], spisok))
+            if 1 in spisok2 and 2 in spisok2:
+                print(f'1) {spisok1[0]} - WINNER 😎🏆')
+                print(f'2) {spisok1[1]} - ROUND-UP 😀')
+                print(f'3) {spisok1[2]} - LOSER 😫')
+                break
+        case 4:
+            P2.level, P2.status=brosok(P2)
+            P2.play=True
+            P3.level, P3.status=brosok(P3)
+            P3.play=True
+            P4.level, P4.status=brosok(P4)
+            P4.play=True
+            spisok=[(P1.name, P1.level, P1.status), (P2.name, P2.level, P2.status), (P3.name, P3.level, P3.status), (P4.name, P4.level, P4.status)]
+            spisok.sort(key=lambda x: x[2], reverse=False)
+            spisok1=list(map(lambda x: x[0], spisok))
+            spisok2=list(map(lambda x: x[2], spisok))
+            if 1 in spisok2 and 2 in spisok2 and 3 in spisok2:
+                print(f'1) {spisok1[0]} - WINNER 😎🏆')
+                print(f'2) {spisok1[1]} - ROUND-UP 😀')
+                print(f'3) {spisok1[2]} - BRONZE MEDALIST 😐')
+                print(f'4) {spisok1[3]} - LOSER 😫')
+                break
